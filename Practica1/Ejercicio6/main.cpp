0f055bc3e50b284f67301d985ebb6605676b7d37
Sebastian-8a <s.ochoa@udea.edu.co>
2023-08-28 16:16:41 -0500

    Solución ejercicio 6

--- Practica1/Ejercicio6/main.cpp
+++ Practica1/Ejercicio6/main.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main(){
 
+    int A = 0;
+    int B = 0;
+    int contador = 1; //En caso de iniciarlo en 0, el ciclo for itera una vez más de las necesarias
+
+    cout << "Ingrese un número A: ";
+            cin >> A ;
+    int total = A;
+    cout<< endl << "Ingrese un número B: ";
+            cin >> B;
+    for (contador; contador < B ; contador++){
+        total *= A;
+        }
+    cout << endl << A << "^" << B << " = " << total;
 
     return 0;
 }
