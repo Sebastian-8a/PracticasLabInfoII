d1b1b9f1bf233a930ee6b48ec4367a32299365e4
Sebastian-8a <s.ochoa@udea.edu.co>
2023-08-28 16:52:55 -0500

    Solución ejercicio 12

--- Practica1/Ejercicio12/main.cpp
+++ Practica1/Ejercicio12/main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main(){
 
+    int A = 0;
+    int potencia = 1;
+    cout << "Ingrese un número A: ";
+            cin >> A ;
+    for (int contador = 1; contador <= 5 ; contador++){
+        potencia *= A ;
+        cout << endl << A << "^" << contador << " = "<< potencia;
+    }
 
     return 0;
 }
