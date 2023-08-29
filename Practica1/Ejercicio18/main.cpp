31383b6347ad0616ced6300dfdb25425f2a3073b
Sebastian-8a <s.ochoa@udea.edu.co>
2023-08-29 07:33:34 -0500

    Solución ejercicio 18

--- Practica1/Ejercicio18/main.cpp
+++ Practica1/Ejercicio18/main.cpp
@@ -4,6 +4,21 @@ using namespace std;
 
 int main(){
 
+    int ingresado = 0;
+    int suma = 0 ;
+    int num = 1;
+    cout << "Ingrese un número: ";
+    cin >> ingresado;
+    while (suma < ingresado){
+        suma += num;
+        num += 2;
+    }
+    if (suma == ingresado){
+        cout << endl << ingresado << " Es un cuadrado perfecto";
+    }
+    else{
+        cout << endl << ingresado << " NO es un cuadrado perfecto";
+    }
 
     return 0;
 }
