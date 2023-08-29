88fab3ba58f6a3cefa5b7ccbb9ab9de298b95ec7
Sebastian-8a <s.ochoa@udea.edu.co>
2023-08-29 07:03:48 -0500

    solución ejercicio 16

--- Practica1/Ejercicio16/main.cpp
+++ Practica1/Ejercicio16/main.cpp
@@ -4,6 +4,22 @@ using namespace std;
 
 int main(){
 
+    int contador = 0;
+    int numIngresado = 1;
+    float  suma = 0;
+
+    while (numIngresado != 0){
+        cout << "Ingrese un número: ";
+        cin >> numIngresado;
+        if (numIngresado != 0){
+            suma += numIngresado;
+            contador++;
+            cout << endl << "El promedio de los números ingresados al momento es: " << (suma/contador);
+                cout << endl << "En caso de desear detenerse ingrese el número 0 " << endl ;
+        }
+    }
+
+
 
     return 0;
 }
