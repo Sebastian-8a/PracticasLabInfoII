23ff24911ea19780c7fc5737b76ce65187ac4a01
Sebastian-8a <s.ochoa@udea.edu.co>
2023-08-29 06:51:02 -0500

    Solución ejercicio 14

--- Practica1/Ejercicio14/main.cpp
+++ Practica1/Ejercicio14/main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main(){
 
+    int numPeque = 1;
+    int numGrande = 50;
+    while (numPeque <= 50){
+        cout << numPeque << "     " << numGrande << endl;
+        numPeque++;
+        numGrande--;
+
+    }
 
     return 0;
 }
