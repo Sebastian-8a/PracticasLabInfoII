e6650dcc5749b824657a076fb15ca48f7aa9225d
Sebastian-8a <s.ochoa@udea.edu.co>
2023-08-30 11:27:32 -0500

    Prueba 1 ejercicio 22

--- Practica1/Ejercicio22/main.cpp
+++ Practica1/Ejercicio22/main.cpp
@@ -2,8 +2,29 @@
 
 using namespace std;
 
+int residuo(int );
+
 int main(){
+
+    int total = 0;
+    int seg = 0,min = 0,horas = 0;
+    cout << "Ingrese la cantidad total de segundos: ";
+    cin >> total;
+    seg = total % 60;
+
+    int nuevoTotal = total - seg;
+    min = residuo(nuevoTotal);
+
 
 
     return 0;
+}
+
+int residuo(int valor){
+    if ((valor / 60 ) > 1){
+        return valor%60;
+    }
+    else{
+        return valor;
+    }
 }
