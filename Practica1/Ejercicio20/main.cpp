fcd40c37e96934d415d561b50127c97d2176163b
Sebastian-8a <s.ochoa@udea.edu.co>
2023-08-30 10:46:05 -0500

    solución ejercicio 20

--- Practica1/Ejercicio20/main.cpp
+++ Practica1/Ejercicio20/main.cpp
@@ -2,8 +2,29 @@
 
 using namespace std;
 
-int main(){
+int invertirNumero(int );
 
+int main() {
+    int num = 0;
+    cout << "Número a evaluar: " ;
+    cin >> num ;
+    if (num == invertirNumero(num)){
+        cout << endl << num << " Es un número palíndromo ";
+    }
+    else {
+        cout << endl << num << " No es un número palíndromo ";
+    }
 
     return 0;
+}
+
+int invertirNumero(int num)
+{
+    int inv=0;
+    while(num>0)
+    {
+        inv=inv * 10 + (num % 10);
+        num = num / 10;
+    }
+    return inv;
 }
