47a79fa6cd582cab35a3daecdb5826d98249c039
Sebastian-8a <s.ochoa@udea.edu.co>
2023-08-31 07:21:20 -0500

    solución ejercicio 26

--- Practica1/Ejercicio26/main.cpp
+++ Practica1/Ejercicio26/main.cpp
@@ -3,6 +3,21 @@
 using namespace std;
 
 int main(){
+    int a = 0, b = 0, c = 0;
+    cout << "Ingrese la medida del primer lado: ";
+    cin >> a;
+    cout << "Ingrese la medida del segundo lado: ";
+    cin >> b;
+    cout << "Ingrese la medida del tercer lado: ";
+    cin >> c;
+    if (((a+b)>c) && ((a+c)>b) && ((c+b)>a)){
+
+        if ((a == b) && (b == c)) cout << "Se forma un triángulo equilátero";
+        else if ( (a != b)&& (b != c) && (a != c)) cout << "Se forma un triángulo escaleno";
+        else cout <<"Se forma un triángulo isósceles ";
+        return 0;
+    }
+    cout << "Las longitudes ingresadas no forman un triángulo.";
 
 
     return 0;
