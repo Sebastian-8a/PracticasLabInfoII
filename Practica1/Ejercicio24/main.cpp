b012360ccd812547428bd6d309308177ee42c6d0
Sebastian-8a <s.ochoa@udea.edu.co>
2023-08-31 06:55:12 -0500

    solución ejercicio 24

--- Practica1/Ejercicio24/main.cpp
+++ Practica1/Ejercicio24/main.cpp
@@ -4,6 +4,24 @@ using namespace std;
 
 int main(){
 
+    int N = 0;
+    cout << "Ingrese el tamaño que desea tenga el cuadrado: " ;
+    cin >> N;
+    for (int contador = 0; contador < N; contador++) cout << "+";
+    for (int contador = 0; contador < (N - 2); contador++){
+        cout << "\n";
+        for (int i = 0; i < N; i ++){
+            if ((i == 0) or (i == N-1) ){
+                cout << "+";
+            }
+            else{
+               cout << " ";
+            }
+        }
+    }
+    cout << "\n";
+    for (int contador = 0; contador < N; contador++) cout << "+";
+
 
     return 0;
 }
