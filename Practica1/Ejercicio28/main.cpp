33c68026958e0c584f45c912c8ae4bd6d4891470
Sebastian-8a <s.ochoa@udea.edu.co>
2023-08-31 07:43:48 -0500

    avance 1 ejercicio 28

--- Practica1/Ejercicio28/main.cpp
+++ Practica1/Ejercicio28/main.cpp
@@ -3,7 +3,22 @@
 using namespace std;
 
 int main(){
+    int N = 0;
+    double suma = 0.0;
+    int denominador = 1;
+    cout << "Ingrese un número para realizar la aproximación: ";
+    cin >> N;
+    for (int contador = 0; contador < N ; contador += 2 ){
 
+        if ((denominador %2)==0) {
+            suma += 1/denominador;
+        }
+        else{
+            suma -= 1/denominador;
+        }
+        denominador += 2;
+    }
+    cout << 4 * suma;
 
     return 0;
 }
