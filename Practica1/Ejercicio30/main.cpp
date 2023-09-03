79211fe4e7c9b8442278da6f0893e8c4f40e7268
Sebastian-8a <s.ochoa@udea.edu.co>
2023-09-03 17:39:21 -0500

    Avance ejercicio 30
    EL problema ya se encuentra resuelto aunque la función rand arroja únicamente el valor de 41 sin importar el caso.

--- Practica1/Ejercicio30/main.cpp
+++ Practica1/Ejercicio30/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-
+#include <cstdlib>
 using namespace std;
 
 int main(){
-
-
+    int ingresado = -1, intentos = 0;
+    int aleatorio = rand() % 101;
+    while (ingresado != aleatorio){
+        intentos++;
+        cout << "\n Ingrese un número: ";
+        cin >> ingresado;
+        if (ingresado < aleatorio) cout << endl << ingresado << " Es menor al número a adivinar";
+                else if (ingresado > aleatorio) cout << endl << ingresado << " Es mayor al número a adivinar";
+    }
+    cout << "\n Has adivinado! \n El número era: " << aleatorio << "\n Te ha tardado " << intentos << " Intentos";
     return 0;
 }
