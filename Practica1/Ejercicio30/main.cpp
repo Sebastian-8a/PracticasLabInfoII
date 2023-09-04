7e6e106d67a26cc7637585af480ce74d9f5505db
Sebastian-8a <s.ochoa@udea.edu.co>
2023-09-04 14:17:39 -0500

    Modificación de carpetas

--- Practica1/Ejercicio30/main.cpp
+++ Practica1/Ejercicio30/main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main(){
     int ingresado = -1, intentos = 0;
     int aleatorio = rand() % 101;
+    cout << aleatorio;
+    /*
     while (ingresado != aleatorio){
         intentos++;
         cout << "\n Ingrese un número: ";
@@ -13,5 +15,6 @@ int main(){
                 else if (ingresado > aleatorio) cout << endl << ingresado << " Es mayor al número a adivinar";
     }
     cout << "\n Has adivinado! \n El número era: " << aleatorio << "\n Te ha tardado " << intentos << " Intentos";
+    */
     return 0;
 }
