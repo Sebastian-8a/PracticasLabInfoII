c79ff35e0310e9158e25df28a307e4e89bf20b59
Sebastian-8a <s.ochoa@udea.edu.co>
2023-09-04 16:22:05 -0500

    Solución ejercicio 30

--- Practica1/Ejercicios/Ejercicio30/main.cpp
+++ Practica1/Ejercicios/Ejercicio30/main.cpp
@@ -8,7 +8,7 @@ int main(){
     srand(seed);
     int ingresado = -1, intentos = 0;
     int aleatorio = rand() % 101;
-    /*
+
     while (ingresado != aleatorio){
         intentos++;
         cout << "\n Ingrese un número: ";
@@ -17,6 +17,6 @@ int main(){
                 else if (ingresado > aleatorio) cout << endl << ingresado << " Es mayor al número a adivinar";
     }
     cout << "\n Has adivinado! \n El número era: " << aleatorio << "\n Te ha tardado " << intentos << " Intentos";
-    */
+
     return 0;
 }
