233ff158dde5bb645a45510844b21b8d847d4d79
Sebastian-8a <s.ochoa@udea.edu.co>
2023-09-08 20:31:44 -0500

    Solución problema 4

--- Practica1/Problemas/Problema4/main.cpp
+++ Practica1/Problemas/Problema4/main.cpp
@@ -1,8 +1,40 @@
-#include <QCoreApplication>
+#include <iostream>
 
-int main(int argc, char *argv[])
+using namespace std;
+
+int main()
 {
-    QCoreApplication a(argc, argv);
+    int temporal = 0, hora1 = 0, hora2 = 0;
+    short int min1 = 0, min2 = 0;
+    int horasTot = 0, minTot = 0;
 
-    return a.exec();
+    cout << "Ingrese el valor de la primera hora: ";
+    cin >> temporal;
+    hora1 = temporal/100;       //Conocer la cantidad de horas
+    if (((temporal - (hora1*100))>= 60)|| (hora1 > 23)){          //Evaluar que no se haya ingresado más de 60 minutos o 24 horas
+        cout << temporal << " es un tiempo inválido.";
+            return 0;
+    }
+    min1 = temporal - (hora1*100);        //Calcular la cantidad de minutos
+    cout << "Ingrese el valor de la segunda hora: ";
+    cin >> temporal;
+    hora2 = temporal/100;
+    if (((temporal - (hora2)*100)>= 60)|| (hora2 > 23)){
+            cout << temporal << " es un tiempo inválido.";
+                return 0;
+    }
+    min2 = temporal - (hora2*100);
+    horasTot = hora1 + hora2;
+    if ((min1 + min2)>= 60){        //Determinar que la posibilidad de que los minutos al sumarse resulten como una hora más
+        horasTot += 1;
+        minTot = (min1 + min2) - 60;
+    }
+    else minTot = min1 + min2;
+    if (horasTot >23) cout << " los valores ingresados son inválidos.";     //En caso de que la suma de los dos valores resulte mayor a las 24 horas del día
+    else
+        cout << " La hora es: " << horasTot << minTot;
+
+
+
+    return 0;
 }
