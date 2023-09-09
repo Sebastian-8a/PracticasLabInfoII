e6bf2893a42ad4cb7b071b5ba32c1e06196889dd
Sebastian-8a <s.ochoa@udea.edu.co>
2023-09-09 18:17:49 -0500

    Solución problema 8

--- Practica1/Problemas/Problema8/main.cpp
+++ Practica1/Problemas/Problema8/main.cpp
@@ -2,14 +2,13 @@
 
 using namespace std;
 
-bool busquedaBinaria(int , int , int , int arreglo[]);
+bool busqueda(int arreglo[], int , int);
 
 int main()
 {
     int num1 = 0, num2 = 0, num3 = 0;
     int multiplosNum1[20] = {}, multiplosNum2[20] ={};      //Delimitarlo ya que al dejarlo sin la cantidad determinada generaba modificación en el for
-    int cantMultiplosNum1 = 0;
-    int inf = 0;
+    int cantMultiplosNum1 = 0, suma = 0;
     cout << "Ingrese el primer número: ";
     cin >> num1;
     cout << "Ingrese el segundo número: ";
@@ -19,33 +18,32 @@ int main()
     for (int i = 1 ; num1 * i < num3; i++){
         multiplosNum1[i] = (num1 * i);
         cantMultiplosNum1 +=1;
+        if (i!= 1)cout << " + "<< multiplosNum1[i] ;
+        else cout << multiplosNum1[i] ;
+        suma += multiplosNum1[i];
     }
-    int sup = cantMultiplosNum1;
     for (int i = 1 ; (num2 * i) < num3; i++){
-        if (busquedaBinaria(inf, sup, (num2*i),multiplosNum1)){
-    cout << "hola mundo";
+        if (busqueda(multiplosNum1, (num2*i),cantMultiplosNum1));       //Analizar si el número ya se encuentra en el otro arreglo
+        else {
+            multiplosNum2[i] = num2 * i;
+            cout << " + "<< multiplosNum2[i];
+            suma += multiplosNum2[i];
         }
-        multiplosNum2[i] = num2 * i;
-    }           //Quise hacer búsqueda binaria, pero ya que no conocía la cantidad de elementos máxima de cada vector, no podía establecer el límite superior
 
+
+    }
+    cout << " = " << suma;
 
 
 
     return 0;
 }
 
-bool busquedaBinaria(int inf, int sup, int num, int arreglo[]){
-    while (inf <= sup){
-        int mitad = (inf + sup) / 2;
-        cout << arreglo[mitad];
-        if (arreglo[mitad] == num ) return true;
-        if (arreglo[mitad] > num ) {
-            sup = mitad;
-            mitad = (inf + sup) / 2;
-        }
-        if (arreglo[mitad] < num ) {
-            inf = mitad;
-            mitad = (inf + sup) / 2;
+bool busqueda(int arreglo[], int num , int len){
+    for (int k = 0; k <= len ; k++){
+
+        if (arreglo[k] == num) {
+            return true;
         }
     }
     return false;
