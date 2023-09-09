40dcce395ebfe6cb1ee2df3695ec8c30b1243b65
Sebastian-8a <s.ochoa@udea.edu.co>
2023-09-09 15:15:45 -0500

    Solución problema 6

--- Practica1/Problemas/Problema6/main.cpp
+++ Practica1/Problemas/Problema6/main.cpp
@@ -1,8 +1,33 @@
-#include <QCoreApplication>
+#include <iostream>
 
-int main(int argc, char *argv[])
+using namespace std;
+
+int factorial(int);
+
+int main()
 {
-    QCoreApplication a(argc, argv);
+    int aprox = 0;
+    float suma = 0;
+    cout << "Ingrese el número de elementos usados en la aproximación: ";
+    cin >> aprox;
+    if (aprox == 0){
+        cout << "e es aproximadamente 0";
+        return 0;
+    }
 
-    return a.exec();
+    for (int j = 0; j < aprox; j++){
+        suma += 1.0/ factorial(j);
+    }
+
+    cout << "e es aproximadamente " << suma;
+
+    return 0;
+}
+
+int factorial(int num){
+    int suma2 = 1;
+    for (int i =1 ; i <= num; i++){
+        suma2 *= i;
+    }
+    return suma2;
 }
