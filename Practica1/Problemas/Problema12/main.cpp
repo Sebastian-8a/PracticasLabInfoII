d5d62f7b6f85710ea83df07214a2e849de31585c
Sebastian-8a <s.ochoa@udea.edu.co>
2023-09-09 21:18:33 -0500

    Solución problema 12

--- Practica1/Problemas/Problema12/main.cpp
+++ Practica1/Problemas/Problema12/main.cpp
@@ -1,8 +1,32 @@
-#include <QCoreApplication>
+#include <iostream>
 
-int main(int argc, char *argv[])
+using namespace std;
+
+int esPrimo(int);
+
+int main()
 {
-    QCoreApplication a(argc, argv);
+    int ingresado = 0;
 
-    return a.exec();
+    cout << "Ingrese un número: ";
+    cin >> ingresado;
+    for (int i =1; i <= ingresado/2;i++){
+        if((ingresado%i)==0){
+            if (esPrimo(ingresado/i)){
+                cout << "El mayor factor primo de "<<ingresado << " es " << ingresado/i;
+                return 0;
+            }
+
+        }
+
+    }
+    return 0;
+}
+
+int esPrimo(int numero){
+    for (int j = 2; j < numero / 2; j++) {
+        if (numero % j == 0) return false;
+    }
+
+    return true;
 }
