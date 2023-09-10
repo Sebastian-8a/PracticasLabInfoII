0f6b12200f9e70729792d05932aab21257b8ab5f
Sebastian-8a <s.ochoa@udea.edu.co>
2023-09-09 20:01:05 -0500

    Solución problema 10

--- Practica1/Problemas/Problema10/main.cpp
+++ Practica1/Problemas/Problema10/main.cpp
@@ -1,8 +1,37 @@
-#include <QCoreApplication>
+#include <iostream>
 
-int main(int argc, char *argv[])
+using namespace std;
+
+int calculoPrimo(int);
+
+int main()
 {
-    QCoreApplication a(argc, argv);
+    int nprimo = 0;
+    int primo = 1;
+    cout << "Ingrese la posición del número primo que desea conocer: ";
+    cin >> nprimo;
+    for (int cant = 0; cant <= nprimo; cant++){
+        if (cant == nprimo){
+            cout << "El primo número "<< nprimo<< " es "<< primo;
+        }
+        primo =  calculoPrimo(primo);
 
-    return a.exec();
+    }
+
+    return 0;
+}
+
+int calculoPrimo(int nuevoPrimo){
+    while(bool bandera = true){
+        nuevoPrimo += 1;
+        for (int i = 2; (i <= ((nuevoPrimo/2)+1))||(i == nuevoPrimo);i++){
+            if ((nuevoPrimo!=i) &&((nuevoPrimo % i)==0)){
+                break;
+            }
+            if ((i == (nuevoPrimo/2))||(i == nuevoPrimo)){
+                return nuevoPrimo;
+            }
+        }
+
+    }
 }
