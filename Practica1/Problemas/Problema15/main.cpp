303ada2dca39af9f6730d226997ce3488e1d78f3
Sebastian-8a <s.ochoa@udea.edu.co>
2023-09-11 06:05:04 -0500

    Solución problema 15

--- Practica1/Problemas/Problema15/main.cpp
+++ Practica1/Problemas/Problema15/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 
-    int n = 0, valor = 0, fila = 0, columna = 0, k = 0;         //k sirve para contar la mitad de los renglones e ir llenando sin reemplazar posiciones con valor ya asignado
+    int n = 0, valor = 0, fila = 0, columna = 0, k = 0, suma = 0;         //k sirve para contar la mitad de los renglones e ir llenando sin reemplazar posiciones con valor ya asignado
     cout << "Ingrese el tamaño de la espiral: ";
     cin >> n;
     valor = (n * n);
@@ -49,15 +49,29 @@ int main()
         columna--;
     }
 
-for (int i = 0; i<n ; i++){
+    for (int i = 0; i<n ; i++){
 
-        for (int j = 0; j<n ; j++){
-            cout<< matriz[i][j]<< " ";
+            for (int j = 0; j<n ; j++){
+                if ((i==n/2 && (j<=(n/2)+1) && (j>=(n/2)-1)) || ((i==(n/2)+1) && (j<=(n/2)+1) && (j>=(n/2)-1))||((i==(n/2)-1)&& (j<=(n/2)+1)&& (j>=(n/2)-1))){
+                    cout<<" "<< matriz[i][j]<< " ";
+                }
+                else
+                cout<< matriz[i][j]<< " ";
+            }
+            cout<< endl;
         }
-        cout<< endl;
-    }
+    valor = 0;
+    for (int m = 0; m< n;m++){
+            suma += matriz[m][m]    ;
+        }
 
 
-
+        for (int q=(n-1); q >= 0; q--){
+            if(q != n/2){
+            suma += matriz[valor][q]    ;
+            }
+            valor++;
+        }
+    cout << "La suma es: "<< suma;
     return 0;
 }
